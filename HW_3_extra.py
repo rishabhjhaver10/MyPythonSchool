bbd4352376fc9cb9f59508d9b0688360859417b0
rishabhjhaver10 <rishabhjhaveri10@gmail.com>
2017-01-13 18:50:01 -0600

    Update HW_3_extra.py

--- HW_3_extra.py
+++ HW_3_extra.py
@@ -1,27 +1,33 @@
 class Textfile():
-
+    #constructor
     def __init__(self, filename):
         self.file = open(filename)
         self.content = self.file.read()
-
+        
+    #returns the number of character in the file
     def nchars(self):
         return len(self.content)
-
+    
+    #returns the number of words in the file
     def nwords(self):
         words = self.content.split()
         return len(words)
-
+    
+    #returns the number of line in the file
     def nlines(self):
         linelist = self.content.split('\n')
         return len(linelist)
-        
+    
+    #returns the test of the file as a string
     def read(self):
         return self.content
-        
+    
+    #returns the lines in a file as a list
     def readlines(self):
         linelist = self.content.split('\n')
         return linelist
-        
+    
+    #takes a string as an input, finds it in the file and returns the index of the input string in the file
     def grep(self, string = ' '):
         linelist = self.content.split('\n')
         linelist1 = []
@@ -31,7 +37,8 @@ class Textfile():
                 print(str(x) + ' : ' + item)
                 linelist1.append(item)
         print(linelist1)
-        
+    
+    #returns the unique words in the file
     def words(self):
         wordlist = self.content.split()
         newlist= []
